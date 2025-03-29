92cc88dc4feb34a6e5645a4d8687b13ecfc80515
nguye <nguye@PHUCMINH>
2025-03-29 13:52:46 +0700

    Add project files.

---
+++ Project1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "toan.h"
+
+int main() {
+	printf("Hay nhap so can tinh tong: ");
+	int d;
+	scanf_s("%d", &d);
+	printf("\n");
+	printf("Hay nhap so can tinh giai thua: ");
+	int b;
+	scanf_s("%d",&b);
+	printf("\n");
+	int c = giaithua(b);
+	int a = sum(d);
+	printf("%d\n", a);
+	printf("%d", c);
+}
+
